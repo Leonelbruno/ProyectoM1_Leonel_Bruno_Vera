367b3732bc2b4eec765c051b296138315b697971
Leonel <leonelbruno972@gmail.com>
2026-05-22 17:41:07 -0300

    Cree e hice funcionar botones Hex, hsl y Generar paleta. Agregue Toasts

--- script.js
+++ script.js
@@ -1,35 +1,140 @@
 /*---------------------------------------------------------
+VARIABLES
+---------------------------------------------------------*/
+const paleta = document.querySelector(".paleta");
+const botones = document.querySelectorAll(".boton-paleta");
+const botonesHslHex = document.querySelectorAll(".botones-hsl-hex")
+const generar = document.querySelector(".boton-generador");
+const toast = document.querySelector(".toast");
+let formatoActual = "hsl";
+let cantidadActual = 6;
+
+/*----------------------------------------------------------
+Funciones Color Random con HSL 
+------------------------------------------------------------*/
+
+function colorAleatorioHsl (){
+    let h = Math.floor((Math.random() * 360));
+    let s = Math.floor((Math.random() * 101));
+    let l = Math.floor((Math.random() * 101));
+
+    return `hsl(${h},${s}%,${l}%)`
+};
+
+console.log(colorAleatorioHsl());
+
+/*----------------------------------------------------------
+Funciones Color Random con hex
+------------------------------------------------------------*/
+
+function colorAleatorioHex (){
+    return '#' + Math.random().toString(16).slice(2, 8)
+};
+
+/*Math.random(): Genera un número decimal aleatorio (por ejemplo: 0.123456...)..toString(16): Convierte ese número a base 16 (hexadecimal), lo que hace aparecer las letras de la a a la f..slice(2, 8): Corta el texto para quedarse únicamente con 6 caracteres después del punto decimal.*/
+
+/*---------------------------------------------------------
+Funcion para crear 1 Tarjeta
+---------------------------------------------------------*/
+
+
+function crearTarjeta() {
+    let colorRandom;
+    if (formatoActual === "hsl"){
+        colorRandom = colorAleatorioHsl();
+    } else {
+        colorRandom = colorAleatorioHex();
+    }
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta-color");
+    const color = document.createElement("div");
+    const hex = document.createElement("p");
+    const hsl = document.createElement("p");
+    color.classList.add("color")
+    tarjeta.appendChild(color);
+    tarjeta.appendChild(hsl);
+    tarjeta.appendChild(hex);
+    paleta.appendChild(tarjeta)
+
+    color.style.backgroundColor = colorRandom;
+    
+    hsl.textContent = colorAleatorioHsl();
+    
+    hex.textContent = colorAleatorioHex();
+
+
+};
+
+/*---------------------------------------------------------
+Funcion para crear paletas.
+---------------------------------------------------------*/
+
+function crearPaleta (num) {
+    paleta.textContent = "";
+    for(let i = 0; i < num ; i++){
+        crearTarjeta();
+    }
+};
+
+
+/*---------------------------------------------------------
 Marcar Boton Seleccionado
 ---------------------------------------------------------*/
-const botones = document.querySelectorAll(".boton-paleta");
 
 botones.forEach(function(boton){
     boton.addEventListener("click", function(){
-
+        
         botones.forEach(function(b){
+
             b.classList.remove("boton-seleccionado");
         });
         
         boton.classList.add("boton-seleccionado");
+
+        let num = Number(boton.textContent);
+
+        cantidadActual = num;
+    })
+})
+
+botonesHslHex.forEach(function(boton){
+    boton.addEventListener("click", function(){
+
+        if (boton.textContent === "HEX"){
+            formatoActual = "hex";
+            mostrarToast("Se generará colores con Hex");
+        } else {
+            mostrarToast("Se generará colores con HSL")
+            formatoActual = "hsl";
+        }
+        botonesHslHex.forEach(function(b){
+            b.classList.remove("boton-formato-seleccionado");
+        });
+
+        boton.classList.add("boton-formato-seleccionado");
+
+        
     })
 })
 
 /*---------------------------------------------------------
-Paletas
+Boton Generar Paleta
+---------------------------------------------------------*/
+generar.addEventListener("click", ()=>{
+    crearPaleta(cantidadActual);
+    mostrarToast("Se Generó una Paleta Aleatoria")
+})
+
+crearPaleta(cantidadActual)
+
+/*---------------------------------------------------------
+Funcion Mostrar y Quitar Toast
 ---------------------------------------------------------*/
 
-const tarjeta = document.createElement("div");
-tarjeta.classList.add("tarjeta-color");
-const color = document.createElement("div");
-color.classList.add("color")
-const hex = document.createElement("p");
-const hsl = document.createElement("p");
-
-tarjeta.appendChild(color);
-tarjeta.appendChild(hex);
-tarjeta.appendChild(hsl);
-
-
-
-
-
+function mostrarToast (mensaje){
+    toast.textContent = mensaje;
+    toast.classList.add("toast-mostrar");
+    setTimeout(function(){
+        toast.classList.remove("toast-mostrar");
+    }, 2000);
+}
